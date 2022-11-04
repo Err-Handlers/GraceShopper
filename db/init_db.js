9fc1c12509d6cde20d8206d9c196a877a2ff93fc
Duke650 <michamcewing@gmail.com>
2022-11-03 20:27:38 -0700

    added dropped tables

--- db/init_db.js
+++ db/init_db.js
@@ -11,18 +11,21 @@ async function buildTables() {
 
     // drop tables in correct order
     await client.query(`
-    DROP TABLE IF EXISTS cart;
+    DROP TABLE IF EXISTS order_pastries;
+    DROP TABLE IF EXISTS cart_pastries;
+    DROP TABLE IF EXISTS orders;
+    DROP TABLE IF EXISTS carts;
     DROP TABLE IF EXISTS reviews;
     DROP TABLE IF EXISTS pastries;
     DROP TABLE IF EXISTS users;
+
   `);
     // build tables in correct order
     await client.query(`
     CREATE TABLE users(
     id SERIAL PRIMARY KEY,
-    email VARCHAR(255) UNIQUE NOT NULL
-    password VARCHAR(255) NOT NULL,
-    
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL
   );
 
   CREATE TABLE pastries(
@@ -62,8 +65,6 @@ async function buildTables() {
     id SERIAL PRIMARY KEY,
     "userId" INTEGER REFERENCES users(id)
    )
-
-
 
    CREATE TABLE reviews(
     id SERIAL PRIMARY KEY,
