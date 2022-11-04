bec6a0b0b827d791a6a7add33b8e0fddb58c2159
Duke650 <michamcewing@gmail.com>
2022-11-03 18:39:00 -0700

    err testing

--- db/init_db.js
+++ db/init_db.js
@@ -2,47 +2,45 @@ const {
   client,
   // declare your model imports here
   // for example, User
-} = require('./');
+} = require("./");
+const { createUsers } = require("./models/user");
 
 async function buildTables() {
   try {
     client.connect();
 
     // drop tables in correct order
-    dropTables()
+    await client.query(`
+    DROP TABLE IF EXISTS pastries;
+    DROP TABLE IF EXISTS users
+  `);
     // build tables in correct order
-    createTables()
-  } catch (error) {
-    throw error;
-  }
-}
-
-async function createTables() {
-  console.log("Starting to create tables...");
-  await client.query(`
-  CREATE TABLE users(
+    await client.query(`
+    CREATE TABLE users(
     id SERIAL PRIMARY KEY,
     username VARCHAR(255) UNIQUE NOT NULL,
-    password VARCHAR(255) NOT NULL
+    password VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL
   );
 
   CREATE TABLE pastries(
     id SERIAL PRIMARY KEY,
     name VARCHAR(255) UNIQUE NOT NULL,
     description TEXT NOT NULL,
-    isGlutenFree BOOLEAN DEFAULT false,
-    isSweet BOOLEAN DEFAULT false,
+    "isGlutenFree" BOOLEAN DEFAULT false,
+    "isSweet" BOOLEAN DEFAULT false,
     price INTEGER
    );
 
-  `)
-}
-
-async function dropTables() {
-  await client.query(`
-    DROP TABLE IF EXISTS pastries;
-    DROP TABLE IF EXISTS users
-  `)
+   CREATE TABLE cart(
+    id SERIAL PRIMARY KEY
+    "userId" INTEGER REFERENCES users(id)
+    "pastriesId" INTEGER REFERENCES pastries(id)
+   );
+  `);
+  } catch (error) {
+    throw error;
+  }
 }
 
 async function populateInitialData() {
@@ -50,6 +48,27 @@ async function populateInitialData() {
     // create useful starting data by leveraging your
     // Model.method() adapters to seed your db, for example:
     // const user1 = await User.createUser({ ...user info goes here... })
+    const createdUsers = [
+      {
+        username: "Kim",
+        password: "kimspassword",
+        email: "kimsemail@email.com"
+      },
+      {
+        username: "Erin",
+        password: "erinspassword",
+        email: "erinsemail@email.com"
+      },
+      {
+        username: "Thuan",
+        password: "thuanspassword",
+        email: "thuansemail@email.com"
+      },
+    ];
+
+    const users = await Promise.all(createdUsers.map(createUsers));
+    console.log("Users being created");
+    console.log(users);
   } catch (error) {
     throw error;
   }
--- db/models/user.js
+++ db/models/user.js
@@ -1,12 +1,28 @@
 // grab our db client connection to use with our adapters
 const client = require('../client');
-
+const bcrypt = require("bcrypt")
 module.exports = {
   // add your database adapter fns here
   getAllUsers,
+  createUser
 };
 
 async function getAllUsers() {
   /* this adapter should fetch a list of users from your db */
-  
+  const { rows } = await client.query(`
+    SELECT * FROM users
+  `)
+  return rows
+}
+
+async function createUser({username, password, email }) {
+  const hashedPassword = await bcrypt.hash(password, 10)
+
+  const { rows: [user] } = await client.query(`
+    INSERT INTO users (username, password, email)
+    VALUES ($1, $2, $3)
+    RETURNING *;
+  `, [username, hashedPassword, email])
+  delete user.password
+  return user;
 }
