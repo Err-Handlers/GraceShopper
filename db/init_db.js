7420999468d8c31a96384e8d258927fef43fc11b
Kim Truong <kimme85@gmail.com>
2022-11-03 20:55:50 -0700

    debugging

--- db/init_db.js
+++ db/init_db.js
@@ -11,69 +11,71 @@ async function buildTables() {
 
     // drop tables in correct order
     await client.query(`
-    DROP TABLE IF EXISTS order_pastries;
-    DROP TABLE IF EXISTS cart_pastries;
-    DROP TABLE IF EXISTS orders;
-    DROP TABLE IF EXISTS carts;
-    DROP TABLE IF EXISTS reviews;
-    DROP TABLE IF EXISTS pastries;
-    DROP TABLE IF EXISTS users;
+      DROP TABLE IF EXISTS order_pastries;
+      DROP TABLE IF EXISTS orders;
+      DROP TABLE IF EXISTS cart_pastries;
+      DROP TABLE IF EXISTS carts;
+      DROP TABLE IF EXISTS reviews;
+      DROP TABLE IF EXISTS pastries;
+      DROP TABLE IF EXISTS users;
+    `);
 
-  `);
     // build tables in correct order
     await client.query(`
-    CREATE TABLE users(
-    id SERIAL PRIMARY KEY,
-    email VARCHAR(255) UNIQUE NOT NULL,
-    password VARCHAR(255) NOT NULL
-  );
+      CREATE TABLE users(
+        id SERIAL PRIMARY KEY,
+        username VARCHAR(255) UNIQUE NOT NULL,
+        email VARCHAR(255) UNIQUE NOT NULL,
+        password VARCHAR(255) NOT NULL
+      );
 
-  CREATE TABLE pastries(
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(255) UNIQUE NOT NULL,
-    description TEXT NOT NULL,
-    "isGlutenFree" BOOLEAN DEFAULT false,
-    "isSweet" BOOLEAN DEFAULT false,
-    "imageURL" VARCHAR(255),
-    inventory INTEGER NOT NULL,
-    "priceInCents" INTEGER NOT NULL
-   );
+      CREATE TABLE pastries(
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(255) UNIQUE NOT NULL,
+        description TEXT NOT NULL,
+        "isGlutenFree" BOOLEAN DEFAULT false,
+        "isSweet" BOOLEAN DEFAULT false,
+        "imageURL" VARCHAR(255),
+        inventory INTEGER NOT NULL,
+        "priceInCents" INTEGER NOT NULL
+      );
 
-   CREATE TABLE carts(
-    id SERIAL PRIMARY KEY,
-    "userId" INTEGER REFERENCES users(id)
-   );
+      CREATE TABLE reviews(
+        id SERIAL PRIMARY KEY,
+        "userId" INTEGER REFERENCES users(id),
+        "pastryId" INTEGER REFERENCES pastries(id),
+        "reviewDescription" TEXT NOT NULL,
+        UNIQUE ("userId", "pastryId")
+      );
 
-   CREATE TABLE cart_pastries (
-    id SERIAL PRIMARY KEY,
-    quantity INTEGER NOT NULL,
-    "pastryId" INTEGER REFERENCES pastries(id),
-    "cartId" INTEGER REFERENCES carts(id),
-    UNIQUE ("cartId", "pastryId")
-   )
+      CREATE TABLE carts(
+        id SERIAL PRIMARY KEY,
+        "userId" INTEGER REFERENCES users(id)
+      );
 
-   CREATE TABLE order_pastries (
-    id SERIAL PRIMARY KEY,
-    quantity INTEGER NOT NULL,
-    "priceInCents" INTEGER NOT NULL,
-    "pastryId" INTEGER REFERENCES pastries(id),
-    "orderId" INTEGER REFERENCES orders(id),
-    UNIQUE ("orderId", "pastryId")
-   )
+      CREATE TABLE cart_pastries(
+        id SERIAL PRIMARY KEY,
+        quantity INTEGER NOT NULL,
+        "pastryId" INTEGER REFERENCES pastries(id),
+        "cartId" INTEGER REFERENCES carts(id),
+        UNIQUE ("cartId", "pastryId")
+      );
 
-   CREATE TABLE orders (
-    id SERIAL PRIMARY KEY,
-    "userId" INTEGER REFERENCES users(id)
-   )
+      CREATE TABLE orders(
+        id SERIAL PRIMARY KEY,
+        "userId" INTEGER REFERENCES users(id)
+      );
 
-   CREATE TABLE reviews(
-    id SERIAL PRIMARY KEY,
-    "userId" INTEGER REFERENCES users(id),
-    "pastryId" INTEGER REFERENCES pastries(id),
-    "reviewDescription" TEXT NOT NULL,
-    UNIQUE ("userId", "pastryId")
-   )
-  `);
+      CREATE TABLE order_pastries(
+        id SERIAL PRIMARY KEY,
+        quantity INTEGER NOT NULL,
+        "priceInCents" INTEGER NOT NULL,
+        "pastryId" INTEGER REFERENCES pastries(id),
+        "orderId" INTEGER REFERENCES orders(id),
+        UNIQUE ("orderId", "pastryId")
+      );
+
+    `);
   } catch (error) {
     throw error;
   }
