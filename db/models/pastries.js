9315b93cd2e0bcc6a21d5d21bc2349c8abbf01c7
Duke650 <michamcewing@gmail.com>
2022-11-09 10:54:11 -0800

    fixed minor errors

--- db/models/pastries.js
+++ db/models/pastries.js
@@ -1,56 +1,84 @@
-
-const client = require('../client');
+const client = require("../client");
 
 async function getAllPastries() {
-    /* this adapter should fetch a list of users from your db */
-    const { rows } = await client.query(`
+  /* this adapter should fetch a list of users from your db */
+  const { rows } = await client.query(`
       SELECT * FROM pastries
-    `)
-    return rows
+    `);
+  return rows;
 }
 
-async function createPastry({name, description, isGlutenFree, isSweet, imageURL, inventory, priceInCents }) {
-
-    const { rows: [pastry] } = await client.query(`
+async function createPastry({
+  name,
+  description,
+  isGlutenFree,
+  isSweet,
+  imageURL,
+  inventory,
+  priceInCents,
+}) {
+  const {
+    rows: [pastry],
+  } = await client.query(
+    `
       INSERT INTO pastries (name, description, "isGlutenFree", "isSweet", "imageURL", inventory, "priceInCents")
       VALUES ($1, $2, $3, $4, $5, $6, $7)
       RETURNING *;
-    `, [name, description, isGlutenFree, isSweet, imageURL, inventory, priceInCents])
-    return pastry;
+    `,
+    [
+      name,
+      description,
+      isGlutenFree,
+      isSweet,
+      imageURL,
+      inventory,
+      priceInCents,
+    ]
+  );
+  return pastry;
 }
 
-async function updatePastry(id, fields){
-  const setString = Object.keys(fields).map(
-    (key, index) => `"${key}"= $${index + 1}`
-  ).join(', ');
-  const {rows: [pastry]} = await client.query(`
+async function updatePastry(id, fields) {
+  const setString = Object.keys(fields)
+    .map((key, index) => `"${key}"= $${index + 1}`)
+    .join(", ");
+  const {
+    rows: [pastry],
+  } = await client.query(
+    `
     UPDATE pastries
     SET ${setString}
     WHERE id=${id}
     RETURNING *;
-  `, Object.values(fields));
+  `,
+    Object.values(fields)
+  );
 
-    return pastry;
+  return pastry;
 }
 
-async function deletePastry({id}){
-  try{
-    const { rows: [pastry]} = await client.query(`
+async function deletePastry({ id }) {
+  try {
+    const {
+      rows: [pastry],
+    } = await client.query(
+      `
       DELETE FROM pastries
       WHERE id = $1
       RETURNING *
-    `, [id]
-  )
+    `,
+      [id]
+    );
 
     return pastry;
-  } catch (error){
-    console.log("deleting patries failed")
+  } catch (error) {
+    console.log("deleting patries failed");
   }
 }
 
 module.exports = {
-    getAllPastries,
-    createPastry,
-    updatePastry,
-    deletePastry
+  getAllPastries,
+  createPastry,
+  updatePastry,
+  deletePastry,
 };
